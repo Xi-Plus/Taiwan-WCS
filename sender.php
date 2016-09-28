5173560b3c57e7ba7828c591a486e51a8057799f
xiplus <huangxuanyuxiplus@gmail.com>
2016-09-29 00:23:01 +0800

    merge from Facebook-Page/Work.Classes.Cancel
    https://github.com/Xi-Plus/Facebook-Page/tree/master/Work.Classes.Cancel

--- sender.php
+++ sender.php
@@ -16,15 +16,18 @@ $html=str_replace(array("\r\n","\n","\t"),"",$html);
 $query = new query;
 $query->table = 'city';
 $city_list = $query->SELECT();
+$all_msg="";
 foreach ($city_list as $city) {
 	echo $city['name'];
 	if (preg_match("/".$city['name']."<\/FONT><\/TD><TD vAlign=center align=left width='70%'.*?>(.*?)<\/TD>/", $html, $match)) {
 		$text=strip_tags($match[1]);
 		if ($text != $city['text']) {
+			$msg = $city['name']." 更新為「".$text."」\n";
+			$all_msg .= $msg;
 			$data[$city['city']] = array(
 				'update'=>true,
 				'name'=>$city['name'],
-				'text'=>$text,
+				'text'=>$msg,
 				'time'=>date("Y-m-d H:i:s")
 			);
 			$query = new query;
@@ -51,7 +54,7 @@ $result = $query->SELECT();
 foreach ($result as $follow) {
 	$city = $follow['city'];
 	if ($data[$city]['update']) {
-		@$messages[$follow['uid']] .= $data[$city]['name']." 更新為「".$data[$city]['text']."」\n";
+		@$messages[$follow['uid']] .= $data[$city]['text'];
 	}
 }
 
@@ -63,7 +66,6 @@ foreach ($messages as $uid => $message) {
 	);
 	$commend = 'curl -X POST -H "Content-Type: application/json" -d \''.json_encode($messageData,JSON_HEX_APOS|JSON_HEX_QUOT).'\' "https://graph.facebook.com/v2.7/me/messages?access_token='.$cfg['page_token'].'"';
 	system($commend);
-	$messages=array();
 	$query = new query;
 	$query->table = 'log';
 	$query->value = array(
@@ -72,5 +74,18 @@ foreach ($messages as $uid => $message) {
 	);
 	$query->INSERT();
 	echo "\n";
+}
+
+if ($all_msg != "") {
+	$all_msg = "資料來源： http://www.dgpa.gov.tw/nds.html\n\n".$all_msg."\n本粉專依據行政院人事行政總處網站發布的內容轉載，一切以來源為準";
+	$commend = 'curl -X POST -H "Content-Type: application/json" -d \'message='.$all_msg.'\' "https://graph.facebook.com/v2.7/'.$cfg['page_id'].'/feed?access_token='.$cfg['page_token'].'"';
+	system($commend);
+	$query = new query;
+	$query->table = 'log';
+	$query->value = array(
+		array('uid', 'post'),
+		array('text', $all_msg)
+	);
+	$query->INSERT();
 }
 ?>
