70feec935758b57725c15260e521181e876fbd4f
xiplus <huangxuanyuxiplus@gmail.com>
2017-04-09 11:30:49 +0800

    htaccess

---
+++ function/timediff.php
@@ -0,0 +1,7 @@
+<?php
+function TimediffFormat($time) {
+	if ($time<60) return $time."秒";
+	if ($time<60*50) return round($time/60)."分";
+	if ($time<60*60*23.5) return round($time/(60*60))."小時";
+	return round($time/(60*60*24))."天";
+}
