6920ce2acdc82c5acdb2c691aae6c88a2849ce6e
xiplus <huangxuanyuxiplus@gmail.com>
2017-07-28 23:50:08 +0800

    show info with city & some parse optimization

--- function/msgparse.php
+++ function/msgparse.php
@@ -1,32 +1,35 @@
 <?php
-function msgparse($msg) {
+function getcityinfo($msg) {
 	global $D;
 	$citylist = $D["citylist"];
 	foreach ($D["cityshortname"] as $city => $temp) {
 		$citylist []= $city;
 	}
 	$regex = "(".implode("|", $citylist).")";
+	if (preg_match($regex, $msg, $m)) {
+		$city = $m[0];
+		if (isset($D["cityshortname"][$city])) {
+			$city = $D["cityshortname"][$city];
+		}
+		return $city."最新的公告是在".date("Y/m/d H:i", strtotime($D["city"][$city]["time"]))."的「".$D["city"][$city]["status"]."」\n".
+			"欲接收訊息通知請輸入 /add ".$city;
+	}
+	return "";
+}
+function mainparse($msg) {
 	if (preg_match("/(幹嘛|怎麼|為何|為什麼|為啥).*(不|沒|沒有).*(放假|停課|停班)/", $msg)) {
 		return "幹嘛問我，去問政府啊";
 	}
 	if (preg_match("/(何|怎麼).*(不).*(放假|停課|停班)/", $msg)) {
 		return "幹嘛問我，去問政府啊";
 	}
-	if (preg_match($regex, $msg, $m)) {
-		$city = $m[0];
-		if (isset($D["cityshortname"][$city])) {
-			$city = $D["cityshortname"][$city];
-		}
-		return $city." 現在的公告是「".$D["city"][$city]["status"]."」(".date("Y/m/d H:i", strtotime($D["city"][$city]["time"])).")\n".
-			"欲接收訊息通知請輸入 /add ".$city;
-	}
-	if (preg_match("/(上課|上班|停課|停班).*[ㄇ嗎？]/", $msg)) {
+	if (preg_match("/(放假|上課|上班|停課|停班).*(ㄇ|嗎|？|\?|了沒)/", $msg)) {
 		return "想知道不會自己去查喔";
 	}
 	if (preg_match("/(為何|為什麼|為啥).*(未達|沒達).*(標準)/", $msg)) {
 		return "想知道不會自己去查喔";
 	}
-	if (preg_match("/(何時|啥時|什麼時候|哪天|哪日|是否).*(放假|停課|停班)/", $msg)) {
+	if (preg_match("/(何時|啥時|什麼時候|哪天|哪日|幾號|是否|今天|明天|後天|有沒有).*(放假|停課|停班)/", $msg)) {
 		return "想知道不會自己去查喔";
 	}
 	if (preg_match("/(幾點|何時|啥時|什麼時候).*(宣布|宣佈|公布|公佈|公告|發布|發佈)/", $msg)) {
@@ -41,13 +44,13 @@ function msgparse($msg) {
 	if (preg_match("/(不).*(宣布|宣佈|公布|公佈|公告|發布|發佈).*[ㄇ嗎？?]/", $msg)) {
 		return "我哪知道，去問政府啊";
 	}
-	if (preg_match("/(宣布|宣佈|公布|公佈|公告|發布|發佈).*了.*[ㄇ嗎？]/", $msg)) {
+	if (preg_match("/(宣布|宣佈|公布|公佈|公告|發布|發佈).*了.*[ㄇ嗎？?]/", $msg)) {
 		return "想知道不會自己去查喔";
 	}
 	if (preg_match("/(幹嘛|怎麼|為何|為什麼|為啥).*(不|還沒|還不|還沒).*(宣布|宣佈|公布|公佈|公告|發布|發佈)/", $msg)) {
 		return "幹嘛問我，去問政府啊";
 	}
-	if (preg_match("/(查的到|能查到).*(嗎|喔|？|?)/", $msg)) {
+	if (preg_match("/(查的到|能查到).*(嗎|喔|？|\?)/", $msg)) {
 		return "查的到";
 	}
 	if (preg_match("/(哪裡).*(可以|能).*(查到)/", $msg)) {
@@ -62,8 +65,14 @@ function msgparse($msg) {
 	if (preg_match("/(幹嘛)/i", $msg)) {
 		return "怎麼了";
 	}
+	if (preg_match("/(好啦|好喔)/i", $msg)) {
+		return "才不好";
+	}
 	if (preg_match("/(幹|靠北|操你)/i", $msg)) {
 		return "幹嘛罵我，去罵政府啊";
+	}
+	if (preg_match("/test/i", $msg)) {
+		return "測試個毛";
 	}
 	if ($msg === "1") {
 		return "2";
@@ -81,4 +90,14 @@ function msgparse($msg) {
 		return "你有病嗎";
 	}
 	return "";
+}
+function msgparse($msg) {
+	$res = mainparse($msg);
+	if (($cityinfo = getcityinfo($msg)) !== "") {
+		if ($res !== "") {
+			$res .= "，我就大發慈悲的告訴你\n\n";
+		}
+		$res .= $cityinfo;
+	}
+	return $res;
 }
